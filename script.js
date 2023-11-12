7306e01e89f14968c047cbbadae7c21705b6e27f
Luiz Henrique <luizhenriques.assis41@gmail.com>
2023-11-12 19:42:21 -0300

    funcao2

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const nome = "JavaScript"
+/*function livro(nome, ano , autor){
+  const nomeMaior = nome.toUpperCase();
+  const total = 2050 - ano;
+  const frase =  nome + "por" + autor ;
+  const objeto = {
+    nome: nomeMaior , 
+    total,
+    frase
+  }
+return objeto;
+}
+*/
 
-function logCurso() {
-  const nome = "HTML"
-  console.log(nome);
-  return nome;
+function livro(nome, ano , autor){
+ 
+return{
+  nome:  nome.toUpperCase() , 
+  total: 2050 - ano,
+  frase: nome + "por" + autor
+};
 }
 
-const nomedaFuncao = logCurso();
+const livroRetorno = livro('O Senhor dos Anes', 1954, 'J.R.R. Tolkien');
 
-console.log(nome);
-console.log(nomedaFuncao);
+console.log(livroRetorno.frase);
