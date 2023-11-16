d7f6492f310d8d0e6b51b260e2f52a1b9160c2a9
Luiz Henrique <luizhenriques.assis41@gmail.com>
2023-11-15 21:49:05 -0300

    Array/Loops3

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const lista = ["JavaScript", 20, true];
+const links = document.querySelectorAll('nav a');
 
-const body = document.querySelector("body");
-
-for (let index = 0; index < lista.length; index++){
-  console.log(lista[index]);
-  body.innerHTML += "<p>" + lista[index] +  "</p>";
+function logHref(item) {
+  const href = item.href;
+  console.log(item);
 }
+
+links.forEach(logHref);
+
+console.log(links);
+
+const lista = ["JasvaScript","HTML","CSS"];
+
+const ultimo = lista.pop();
+const primeiro = lista.shift();
+
+console.log(lista)
+console.log(ultimo);
+console.log(primeiro)
